79337983a00b1e83b4a5684f28ad833e52bbcffd
DanialBarteh <daniallbartehofficial@gmail.com>
2023-05-29 22:40:58 +0330

    V1

---
+++ README.md
@@ -0,0 +1,20 @@
+<p style="text-align: center;margin-bottom: -50px">
+    <a href="https://dabram.ir/resume" target="_blank"><img src="https://dabram.ir/file/image/rezome/perseneli.jpg" height="100" alt="Daniel Barteh"></a>
+</p>
+<br><br>
+<center>Daniel Barteh</center>
+<center>
+<a href="https://dabram.ir/resume">Full Resume</a>
+&nbsp;&nbsp;
+<a href="https://github.com/DanielBarteh">GitHub</a>
+&nbsp;&nbsp;
+<a href="https://linkedin.com/in/DanielBarteh">LinkedIn</a>
+</center>
+<hr>
+
+## About This Project (Camera Calibration)
+* This project made with Python & cv2 package.
+* Package are in venv folders.
+
+## Extra Help
+**You can add image to 'image' folder and link it to python files**
---
+++ picture_01.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(7,7,0)
+objp = np.zeros((7 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+images = glob.glob('Image/01.jpg')
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7, 7), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(0)
+cv.destroyAllWindows()
